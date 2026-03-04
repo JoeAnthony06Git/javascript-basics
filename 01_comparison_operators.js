34ceb582e32a0acd57adc6f4d1b376454b041cff
Anthony Olivares <olivaresanthony67@gmail.com>
2026-03-03 20:57:05 -0500

    feat: demonstrate JavaScript comparison operators

---
+++ 01_comparison_operators.js
@@ -0,0 +1,28 @@
+
+// Comparison operators
+
+// Greater than or Greater than or equal to
+let a = 10;
+let b = 5;
+
+console.log(a > b);   // true
+console.log(b >= a);  // false
+
+// Less than or Less than or equal to
+console.log(a < b);   // false
+console.log(b <= a);  // true
+
+// Equality
+let c = 10;
+let d = '10';
+
+console.log(a == c);  // true (value comparison)
+console.log(a === c); // true (value and type comparison)
+console.log(a == d);  // true (value comparison)
+console.log(a === d); // false (value and type comparison)
+
+// Inequality
+console.log(a != c);  // false (value comparison)
+console.log(a !== c); // false (value and type comparison)
+console.log(a != d);  // false (value comparison)
+console.log(a !== d); // true (value and type comparison)
